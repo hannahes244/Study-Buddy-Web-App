9d90e6cf3996ffaf3cdfbf057f07b36fe9c709a1
Jeremiah Whitehurst <101281773+Jeremiah-A-WV@users.noreply.github.com>
2025-03-26 17:30:03 -0400

    Revert "15 migration to backend"

--- server/models/Class.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const ClassSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  students: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }] // Users taking this class
-});
-
-module.exports = mongoose.model("Class", ClassSchema);
