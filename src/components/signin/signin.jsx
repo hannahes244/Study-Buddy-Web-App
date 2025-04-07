cca36897173692e5c136a247c6a0417e07f04e63
Hannah Sanders <hesanders23@Hannahs-MacBook-Air.local>
2025-04-07 14:17:23 -0400

    done

--- src/components/signin/signin.jsx
+++ src/components/signin/signin.jsx
@@ -2,47 +2,39 @@ import React, {useState} from "react";
 import Popup from 'reactjs-popup';
 import "./signin.css";
 import 'reactjs-popup/dist/index.css';
+import  {Link} from 'react-router-dom';
 
-// function SignIn() {
-//     const [open, setOpen] = useState(false);
-//     const closeModal = () => setOpen(false);
-
-//     return (
-//         <div>
-//           <button onClick={() => setOpen(true)}>Open Popup</button>
-//           <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-//             <div className="modal">
-//               <button className="close" onClick={closeModal}>
-//                 &times;
-//               </button>
-//               {/* Popup content here */}
-//               <span>This is a popup</span>
-//             </div>
-//           </Popup>
-//         </div>
-//     );
-
-// }
-
-// export default SignIn;
 
 const SignInPopup = ({ onClose }) => {
     return (
-      <div className="sipopup-overlay">
-        <div className="sipopup-content">
-          <button id="signInX" onClick={onClose}>x</button>
-
-            <h2 id="signInTitle">Sign In</h2>
-
-            <div className="siInputDiv">
-                <input type="text" placeholder="Email" required/>
-                <input type="password" placeholder="Password" required/>
+      <div className="popup-overlay">
+        <div className="popup-content">
+          <button onClick={onClose} className="SignUpClose">Close</button>
+          <div className="signupcontent">
+            <h2 className="question">Welcome Back!!</h2>
+            <h2 lassName="SignUp">Sign In</h2>
+              <input type="text" placeholder="Email" required />
+              <input type="password" placeholder="Password" required />
             </div>
 
-            <button id="siLogin">Login</button>
-            
+              <Link to="/dashboard" onClick={onClose} className="SignUpLogin">Login</Link>
         </div>
       </div>
+      // <div className="sipopup-overlay">
+      //   <div className="sipopup-content">
+      //     <button id="signInX" onClick={onClose}>Close </button>
+
+      //       <h2 id="signInTitle">Sign In</h2>
+
+      //       <div className="siInputDiv">
+      //           <input type="text" placeholder="Email" required/>
+      //           <input type="password" placeholder="Password" required/>
+      //       </div>
+
+      //       <button id="siLogin">Login</button>
+            
+      //   </div>
+      // </div>
     );
   };
   
--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -2,16 +2,22 @@ import React, {useState} from "react";
 import Popup from 'reactjs-popup';
 import "./signup.css";
 import 'reactjs-popup/dist/index.css';
+import  {Link} from 'react-router-dom';
 
-const SignUpPopup = ({ onClose }) => {
+const SignUpPopup = ({ onClose}) => {
+
     return (
       <div className="popup-overlay">
         <div className="popup-content">
-          <h2>Sign In</h2>
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button>Login</button>
-          <button onClick={onClose}>Close</button>
+          <button onClick={onClose} className="SignUpClose">Close</button>
+          <div className="signupcontent">
+            <h2 className="question">Want to find Study Buddies?</h2>
+            <h2 lassName="SignUp">Sign Up</h2>
+              <input type="text" placeholder="Email" required />
+              <input type="password" placeholder="Password" required />
+            </div>
+
+              <Link to="/accountsettings" onClick={onClose} className="SignUpLogin">Sign Up</Link>
         </div>
       </div>
     );
