811c2a3b62d9a100bd407c860e053b50e160d0d3
Saniyah Bullock <slbullock23@students.desu.edu>
2025-04-11 18:55:16 -0400

    all commits

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ import AccountSettings from "./pages/accountsettings/accountsettings";
 import Footer from "./components/footer/footer";
 import Dashboard from "./pages/dashboard/dashboard";
 import FindMatches from "./pages/findmatches/findmatches";
-import Requests from "./pages/requests/requests";
 import MyMatches from "./pages/mymatches/mymatches";
 import Resources from "./pages/resources/resources";
-
+import Blog from "./pages/blog/blog";
+import About from "./pages/about/about";
+import Faq from "./pages/faq/faq"
 
 function App() {
   return (
     <Router>
       <NavBar />
-      
-      {/* Ensure dashboard is not rendered directly! */}
       <Routes>
         <Route path="/" element={<Dashboard />} />  
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/findmatches" element={<FindMatches />} />
-        <Route path="/requests" element={<Requests />} />
         <Route path="/mymatches" element={<MyMatches />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/accountsettings" element={<AccountSettings />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<Faq />} />
       </Routes>
-
       <Footer />
     </Router>
   );
