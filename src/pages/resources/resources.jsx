72ec6d1b14fb98a5380b19488fb749272a8a6912
Justjavon2 <Jmedmunds23@studnents.desu.edu>
2025-04-14 15:36:11 -0400

    Resources Update

--- src/pages/resources/resources.jsx
+++ src/pages/resources/resources.jsx
@@ -3,11 +3,41 @@ import "./resources.css"
 
 const Resources = () => {
     return (
-        <article className="resourcesArticle">
-            <div>
-                <h1>Resources: Insert Page Elements Here!</h1>
-            </div>
-        </article>
+      <article className="resourcesContainer">
+        <h2 className="resources-offer-title">Study Resources</h2>
+        <div className="resources-offer-contents">
+          
+          <table>
+            <tbody><tr>
+                <th>Name </th>
+                <th>Link</th>
+              </tr>
+              <tr>
+                <td>Textbooks:</td>
+                <td><a href="https://lib.de.us/emedia/?gad_source=1&gclid=Cj0KCQjwm7q-BhDRARIsACD6-fUxrL6hUcUwZWG6zErnTIbYAV5iOgmcSHb67sE2Jub00RGwhsdmR_QaAjuuEALw_wcB"> Dsu Education</a></td>
+              </tr>
+              <tr>
+                <td>Study Recources:</td>
+                <td><a href="https://www.desu.edu/student-life/athletics/academic-services/study-tips-strategies">Dsu Recources</a></td>
+              </tr>
+              <tr>
+                <td>Campus Map:</td>
+                <td><a href="https://www.desu.edu/about/location-directions">Campus Map</a></td>
+              </tr>
+              <tr>
+                <td>Discord Study Lounges :</td>
+                <td><a href="https://discord.com/app">Discord</a></td>
+              </tr>
+              <tr>
+                <td>Dsu Hub:</td>
+                <td><a href="https://www.desu.edu/news/2022/08/university-launches-all-inclusive-campus-app-dsu-hub">  Campus Hub</a></td>
+              </tr>
+              <tr>
+              </tr>
+            </tbody>
+            </table>
+        </div>
+      </article>
     )
 }
 
