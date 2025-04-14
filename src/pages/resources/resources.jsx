9f52ecf6c8b05c197b17cb0638d3d686ea28ce68
Justjavon2 <Jmedmunds23@studnents.desu.edu>
2025-04-14 16:00:50 -0400

    Resources Update

--- src/pages/resources/resources.jsx
+++ src/pages/resources/resources.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./resources.css"
 
 const Resources = () => {
+<<<<<<< Updated upstream
     return (
       <article className="resourcesContainer">
         <h2 className="resources-offer-title">Study Resources</h2>
@@ -39,6 +40,45 @@ const Resources = () => {
         </div>
       </article>
     )
+=======
+    return (
+      <article className="resourcesContainer">
+        <h2 className="resources-offer-title">Study Resources</h2>
+        <div className="resources-offer-contents">
+          
+          <table>
+            <tbody><tr>
+                <th>Name </th>
+                <th>Link</th>
+              </tr>
+              <tr>
+                <td>Textbooks:</td>
+                <td><a href="https://lib.de.us/emedia/?gad_source=1"> Dsu Education</a></td>
+              </tr>
+              <tr>
+                <td>Study Resources:</td>
+                <td><a href="https://www.desu.edu/student-life/athletics/academic-services/study-tips-strategies">Dsu Resources</a></td>
+              </tr>
+              <tr>
+                <td>Campus Map:</td>
+                <td><a href="https://www.desu.edu/about/location-directions">Campus Map</a></td>
+              </tr>
+              <tr>
+                <td>Discord Study Lounges :</td>
+                <td><a href="https://discord.com/app">Discord</a></td>
+              </tr>
+              <tr>
+                <td>Dsu Hub:</td>
+                <td><a href="https://www.desu.edu/news/2022/08/university-launches-all-inclusive-campus-app-dsu-hub">  Campus Hub</a></td>
+              </tr>
+              <tr>
+              </tr>
+            </tbody>
+            </table>
+        </div>
+      </article>
+    )
+>>>>>>> Stashed changes
 }
 
 export default Resources;
